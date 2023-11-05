f9484e9771ff03134cebd4212b3d0193e0fcf91e
Samiya Kazi <samiya.kazi09@gmail.com>
2023-11-05 20:18:22 +0600

    refactor: styled form

--- client/src/Pages/FormPage.tsx
+++ client/src/Pages/FormPage.tsx
@@ -1,3 +1,4 @@
+import { DollarOutlined } from "@ant-design/icons";
 import { Button, Form, Input } from "antd"
 import axios from "axios";
 import { useState } from "react"
@@ -8,7 +9,7 @@ function FormPage() {
   const [amount, setAmount] = useState<string>('0');
   const [customerName, setCustomerName] = useState<string>('');
   const [customerEmail, setCustomerEmail] = useState<string>('');
-  const [customerPhone, setcustomerPhone] = useState<string>('');
+  const [customerPhone, setCustomerPhone] = useState<string>('');
 
   async function handleSubmit () {
     const data = { orderId, amount, customerName, customerEmail, customerPhone };
@@ -24,26 +25,28 @@ function FormPage() {
 
 
   return (
-    <Form onFinish={handleSubmit}>
-      <h1>Make Payment</h1>
-      <Form.Item label="Order ID" required>
-        <Input type="text" onChange={(val) => setOrderId(val.target.value)}/>
-      </Form.Item>
-      <Form.Item label="Amount" required>
-        <Input type="number" onChange={(val) => setAmount(val.target.value)}/>
-      </Form.Item>
-      <Form.Item label="Customer Name" required>
-        <Input type="text" onChange={(val) => setCustomerName(val.target.value)}/>
-      </Form.Item>
-      <Form.Item label="Customer Email" required>
-        <Input type="email" onChange={(val) => setCustomerEmail(val.target.value)}/>
-      </Form.Item>
-      <Form.Item label="Customer Phone" required>
-        <Input type="tel" onChange={(val) => setcustomerPhone(val.target.value)}/>
-      </Form.Item>
+    <div className="form-container">
+      <Form onFinish={handleSubmit} className="form">
+        <h1>Make Payment</h1>
+        <Form.Item label="Order ID" required>
+          <Input type="text" onChange={(val) => setOrderId(val.target.value)}/>
+        </Form.Item>
+        <Form.Item label="Amount" required>
+          <Input type="number" onChange={(val) => setAmount(val.target.value)}/>
+        </Form.Item>
+        <Form.Item label="Customer Name" required>
+          <Input type="text" onChange={(val) => setCustomerName(val.target.value)}/>
+        </Form.Item>
+        <Form.Item label="Customer Email" required>
+          <Input type="email" onChange={(val) => setCustomerEmail(val.target.value)}/>
+        </Form.Item>
+        <Form.Item label="Customer Phone" required>
+          <Input type="tel" onChange={(val) => setCustomerPhone(val.target.value)}/>
+        </Form.Item>
 
-      <Button type="primary" htmlType="submit">Pay with SSL Commerz</Button>
-    </Form>
+        <Button type="primary" htmlType="submit" size="large" icon={<DollarOutlined />}>Pay with SSL Commerz</Button>
+      </Form>
+    </div>
   )
 }
 
